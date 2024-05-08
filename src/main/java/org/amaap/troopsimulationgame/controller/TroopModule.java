7cd05b4a522e7844feafb892990f4794f1f9d19b
amey1302 <ameykulkarni1322@gmail.com>
2024-05-09 00:14:29 +0530

    Add Module for depedency Injection

---
+++ src/main/java/org/amaap/troopsimulationgame/controller/TroopModule.java
@@ -0,0 +1,10 @@
+package org.amaap.troopsimulationgame.controller;
+
+import com.google.inject.AbstractModule;
+
+public class TroopModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
