b21e24170f5336ab45b0915d2eddca9ba1c0c761
amey1302 <ameykulkarni1302@gmail.com>
2024-06-21 00:03:08 +0530

    Add InvalidTrainingTimeException

---
+++ src/main/java/org/amaap/troopsimulationgame/service/exception/InvalidTroopTrainingTimeException.java
@@ -0,0 +1,7 @@
+package org.amaap.troopsimulationgame.service.exception;
+
+public class InvalidTroopTrainingTimeException extends InvalidTroopDataException {
+    public InvalidTroopTrainingTimeException(String message) {
+        super(message);
+    }
+}
